21b7234941b32783172c7237c7c05a02517af507
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-06-19 18:02:55 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import About from "./components/about/About.jsx"
-import Contact from "./components/contact/Contact.jsx"
-import Experience from "./components/experience/experience.jsx"
-import Hero from "./components/hero/Hero.jsx"
-import NavBar from "./components/navbar/NavBar.jsx"
-import Projects from "./components/projects/Projects.jsx"
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import NavBar from "./components/navbar/NavBar.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Experience from "./components/experience/experience.jsx";
 
 function App() {
   
